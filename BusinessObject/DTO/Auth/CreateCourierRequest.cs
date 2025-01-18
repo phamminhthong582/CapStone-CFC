055751ca4d1637a2bb9bcccb2f7d90cb46bd9b72
MinhThong09 <phamminhthong582@gmail.com>
2025-01-18 22:21:32 +0700

    commit

---
+++ BusinessObject/DTO/Auth/CreateCourierRequest.cs
@@ -0,0 +1,33 @@
+﻿namespace BusinessObject.DTO.Auth;
+
+public class CreateCourierRequest
+{
+    public string? FullName { get; set; }
+
+    public string? Address { get; set; }
+
+    public string Password { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public bool? Gender { get; set; }
+
+    public DateTime? Birthday { get; set; }
+
+    public string? IdentificationNumber { get; set; }
+
+    public string? IdentificationFontOfPhoto { get; set; }
+
+    public string? IdentificationBackOfPhoto { get; set; }
+    
+    public string? NumberMoto { get; set; }
+    
+    public string? ColorMoto {  get; set; }
+    
+    public string? MotoType { get; set; }   
+    
+    public bool? Status { get; set; }
+
+}
---
+++ Repository/Interface/ICustomerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Repository.Interface;
+
+public interface ICustomerRepository
+{
+    
+}
