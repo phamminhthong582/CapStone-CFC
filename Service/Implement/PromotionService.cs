4e4572b039a4c04ea17f2adab2c159a662b866b9
MinhThong09 <phamminhthong582@gmail.com>
2025-02-19 09:57:23 +0700

    update code

--- Service/Implement/PromotionService.cs
+++ Service/Implement/PromotionService.cs
@@ -22,26 +22,26 @@ public class PromotionService : IPromotionService
         var list = await _promotionRepository.GetAllPromotion();
         return _mapper.Map<List<PromotionResponse>>(list);
     }
-    public async Task<Result<string>> CheckAndUpdateExpiredPromotions()
-    {
-        var expiredPromotions = await _promotionRepository.GetAllPromotion();
-        int updatedCount = 0;
-
-        foreach (var promotion in expiredPromotions)
-        {
-            if (promotion.EndDate <= DateTime.UtcNow && promotion.Status == true)
-            {
-                promotion.Status = false;
-                await _promotionRepository.UpdatePromotion(promotion);
-                updatedCount++;
-            }
-        }
-        return new Result<string>
-        {
-            ResultStatus = ResultStatus.Success.ToString(),
-            Messages = new[] { $"{updatedCount} promotions were updated successfully." }
-        };
-    }
+    // public async Task<Result<string>> CheckAndUpdateExpiredPromotions()
+    // {
+    //     var expiredPromotions = await _promotionRepository.GetAllPromotion();
+    //     int updatedCount = 0;
+    //
+    //     foreach (var promotion in expiredPromotions)
+    //     {
+    //         if (promotion.EndDate <= DateTime.UtcNow && promotion.Status == true)
+    //         {
+    //             promotion.Status = false;
+    //             await _promotionRepository.UpdatePromotion(promotion);
+    //             updatedCount++;
+    //         }
+    //     }
+    //     return new Result<string>
+    //     {
+    //         ResultStatus = ResultStatus.Success.ToString(),
+    //         Messages = new[] { $"{updatedCount} promotions were updated successfully." }
+    //     };
+    // }
     
 
     public async Task<Result<Promotion>> CreatePromotion(CreatePromotionRequest request)
